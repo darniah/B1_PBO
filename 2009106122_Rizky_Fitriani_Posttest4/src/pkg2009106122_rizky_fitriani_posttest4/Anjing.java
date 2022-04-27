9afcb722f84a93558b0aa2e6a110b32a8d320328
RizkyAmalia1125 <102619211+RizkyAmalia1125@users.noreply.github.com>
2022-04-27 19:53:27 +0800

    Update Anjing.java

--- 2009106122_Rizky_Fitriani_Posttest4/src/pkg2009106122_rizky_fitriani_posttest4/Anjing.java
+++ 2009106122_Rizky_Fitriani_Posttest4/src/pkg2009106122_rizky_fitriani_posttest4/Anjing.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg2009106122_rizky_fitriani_posttest4;
+package praktikum4;
 
-/**
- *
- * @author NEC
- */
-public class Anjing {
+public class Anjing extends PetShop {
+    private String petId;
+    private String Nama;
+    private int tanggalLahir;
+    private double beratBadan;
     
+    public Anjing(PetShop petshop,String petId, String Nama, int tanggalLahir, double beratBadan) {
+        super(petshop.getNo(), petshop.getMerk(), petshop.getJenis(), petshop.getBerat(), petshop.getHarga(), petshop.getKategori());
+        this.petId = petId;
+        this.Nama = Nama;
+        this.tanggalLahir = tanggalLahir;
+        this.beratBadan = beratBadan;
+    }
+    
+    
+    public Anjing(String petId, String Nama, int tanggalLahir, double beratBadan) {
+       super();
+       this.petId = petId;
+       this.Nama = Nama;
+       this.tanggalLahir = tanggalLahir;
+       this.beratBadan = beratBadan;
+       }
+
+    public Anjing((int No, String Merk, String Jenis, double Berat, double Harga, String Kategori) {
+        super(No, Merk, Jenis, Berat, Harga, Kategori);
+    }
+
+    public String getPetId() {
+        return petId;
+    }
+
+    public void setPetId(String petId) {
+        this.petId = petId;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public int getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(int tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public double getBeratBadan() {
+        return beratBadan;
+    }
+
+    public void setBeratBadan(double beratBadan) {
+        this.beratBadan = beratBadan;
+    }
 }
+    
