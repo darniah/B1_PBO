a5a343e8b65c5916f6c238c30e217ba99f8cfef9
Adrian-Tsm <102124748+Adrian-Tsm@users.noreply.github.com>
2022-05-18 14:59:29 +0800

    Postest 6 112

---
+++ 2009106112_AdrianTasmin/src/Postest6/Pulsa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postest6;
+
+import Postest5.*;
+import Postest4.*;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class Pulsa extends Postest3{
+    public static void pulsa(){
+        System.out.println("Kelas Pulsa");
+    }
+    
+}
